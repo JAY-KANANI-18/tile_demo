2d32ac36f7d83c8c5a0829b6d93d70a11d34c6a0
Jay-kanani18 <jkanani.elluminati@gmail.com>
2023-12-05 00:07:37 +0530

    set up

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ app = Flask(__name__)
 CORS(app)
 
 image_list = Load_Data().from_folder(['./main_carpet'])
-st = Search_Setup(image_list=image_list, model_name='vgg19', pretrained=True, image_count=0)
+st = Search_Setup(image_list=image_list, model_name='vgg19', pretrained=True, image_count=1)
 st.run_index(True)
 
 @app.route('/init_data')
